1e119392048bf78c908d4f2c1148513052e2fe07
Dmytro Sirko <dimasirko@gmail.com>
2024-03-16 23:05:46 +0200

    fix: add comma, HT6

--- courses_app/members_app/urls.py
+++ courses_app/members_app/urls.py
@@ -3,7 +3,7 @@ from .views import MemberInputView, member_data_view, SessionView, UserEnrollmen
 
 urlpatterns = [
     path('', MemberInputView.as_view(), name='member_input'),
-    path('member-data/', member_data_view, name='member_data')
+    path('member-data/', member_data_view, name='member_data'),
     path('sessions/', SessionView.as_view(), name='sessions-view'),
     path('user-enrollment/', UserEnrollmentView.as_view(), name='user_enrollment'),
 ]
